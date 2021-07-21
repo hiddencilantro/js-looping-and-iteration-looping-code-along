fe29de2d2a774110e8c94925e9b5ad573b407c34
hiddencilantro <stevekim1109@gmail.com>
2021-07-21 15:21:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let result = [];
+function writeCards(string_names, event_name) {
+    for (let i = 0; i < string_names.length; i++) {
+        result.push(`Thank you, ${string_names[i]}, for the wonderful ${event_name} gift!`);
+    }
+    return result;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
